7e6ed22ee7465cea0a8e05a591a5301a30232727
Jen-Chieh Chiang <jchiang36@gatech.edu>
2019-01-09 16:00:50 -0500

    created server

---
+++ client/build/precache-manifest.cbc47a9190d2a55db684e8c140ed4822.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/static/media/slide_two.3a3ffd36.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a043edd1e0d545df589a",
+    "url": "/static/js/main.a043edd1.chunk.js"
+  },
+  {
+    "revision": "15a0971ae7aa7ba144ce",
+    "url": "/static/js/1.15a0971a.chunk.js"
+  },
+  {
+    "revision": "a043edd1e0d545df589a",
+    "url": "/static/css/main.3eb89e7e.chunk.css"
+  },
+  {
+    "revision": "9bed4b2742ddad214468198ea28f5729",
+    "url": "/index.html"
+  }
+];
